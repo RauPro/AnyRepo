7d8c7e2692bd3222c11c4d034df5eb4049cd629b
RauPro <00012119@uca.edu.sv>
2025-05-04 10:12:34 +0700

    Add comments in models.py

--- models/models.py
+++ models/models.py
@@ -3,14 +3,14 @@ from ipv8.messaging.payload_dataclass import DataClassPayload
 from dataclasses import dataclass
 
 @dataclass
-class TransferTx(DataClassPayload[1]):
+class TransferTx(DataClassPayload[1]): # Normal transaction
     sender: str
     recipient: str
     token: str
     amount: int
 
 @dataclass
-class AddLiquidityTx(DataClassPayload[2]):
+class AddLiquidityTx(DataClassPayload[2]): # Add liquidity from a provider to token_b
     provider: str
     token_a: str
     amount_a: int
@@ -18,7 +18,7 @@ class AddLiquidityTx(DataClassPayload[2]):
     amount_b: int
 
 @dataclass
-class SwapTx(DataClassPayload[3]):
+class SwapTx(DataClassPayload[3]): # Swap from token_x to token_y
     trader: str
     from_token: str
     to_token: str
@@ -30,4 +30,4 @@ class BlockMessage(DataClassPayload[4]):
     prev_hash: str
     timestamp: float
     block_hash: str
-    tx_data: str
+    tx_data: str # JSON-encoded list of transactions in the block

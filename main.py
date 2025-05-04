b516f869bb01b085b0a5b0aa015fb27b7fcac830
RauPro <00012119@uca.edu.sv>
2025-05-04 11:16:56 +0700

    add runner in main.py

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
-from asyncio import run
-from ipv8.configuration import (
-    ConfigBuilder,
-    Strategy,
-    WalkerDefinition,
-    default_bootstrap_defs,
-)
-from ipv8.util import run_forever
+import asyncio
+from ipv8.configuration import ConfigBuilder, WalkerDefinition, Strategy, default_bootstrap_defs
 from ipv8_service import IPv8
-from communities.our_community import OurCommunity
+from communities import TokenCommunity
 
-
-async def start_communities() -> None:
-    for i in [1, 2, 3]:
+async def main():
+    roles = ["miner", "sniper", "user", "whale"]
+    builders = []
+    for i, role in enumerate(roles, start=1):
         builder = ConfigBuilder().clear_keys().clear_overlays()
-
-        builder.add_key("my peer", "medium", f"keys/ec{i}.pem")
-
-        builder.add_overlay(
-            "OurCommunity",
-            "my peer",
-            [WalkerDefinition(Strategy.RandomWalk, 10, {"timeout": 3.0})],
-            default_bootstrap_defs,
-            {},
-            [("started",)],
-        )
-
-        await IPv8(
-            builder.finalize(), extra_communities={"OurCommunity": OurCommunity}
-        ).start()
-
-    await run_forever()
-
-
-run(start_communities())
+        builder.add_key("my peer", "medium", f"ec{i}.pem")
+        builder.add_overlay("TokenCommunity", "my peer",
+                            [WalkerDefinition(Strategy.RandomWalk, 10, {'timeout': 3.0})],
+                            default_bootstrap_defs,
+                            {"role": role}, [('started',)])
+        builders.append(builder)
+    ipv8_instances = []
+    for builder in builders:
+        ipv8 = await IPv8(builder.finalize(), extra_communities={"TokenCommunity": TokenCommunity}).start()
+        ipv8_instances.append(ipv8)
+    await asyncio.Future()
+asyncio.run(main())
